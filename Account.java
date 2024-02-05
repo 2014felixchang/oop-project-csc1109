48ebc3137d7350e3d3800f37f76623f79d1373d1
Leo Oh <leooh29@gmail.com>
2024-02-05 18:21:55 +0800

    Test commit, Added some attributes to account class

--- Account.java
+++ Account.java
@@ -1,3 +1,17 @@
+/*
+ * Account class:
+ * - account info handling (display & manage acc info like acc numbers and types)
+ * - balance management (check balance, show current, remaining, available)
+ * - account operations (deposit, withdraw)
+ */
+
 public class Account {
+    private int numOfAccount;
+    private int[] accounts;
+    private int accBalance;
     
+    public Account() {
+        int numOfAccount = 0;
+        int accBalance = 0;
+    }
 }
