33a4338c552cd07c3b4a6efafd023c4a49cc6842
jonathanljc <2300923@sit.singaporetech.edu.sg>
2024-02-05 18:21:37 +0800

    hello

--- Main.java
+++ Main.java
@@ -8,5 +8,7 @@ public class Main{
         System.out.println("Test");
         System.out.println("Hello!");
         System.out.println("Test");
+        System.out.println("Hello!");
+        System.out.println("Test");
     }
 }
