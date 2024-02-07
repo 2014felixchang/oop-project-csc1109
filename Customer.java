c52a34e80427052ddff281aa5293e8b16c66bbbb
jonathanljc <2300923@sit.singaporetech.edu.sg>
2024-02-07 11:10:31 +0800

    update

--- Customer.java
+++ Customer.java
@@ -1,3 +1,31 @@
 public class Customer {
-    private int YAY;
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private String email;
+    private String dateOfBirth;
+
+    public Customer(String name,String address,String phoneNumber,String email,String dateOfBirth){
+        this.name=name;
+        this.address=address;
+        this.phoneNumber=phoneNumber;
+        this.email=email;
+        this.dateOfBirth=dateOfBirth;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getAddress(){
+        return address;
+    }
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public String getdateOfBirth(){
+        return dateOfBirth;
+    }
+
 }
