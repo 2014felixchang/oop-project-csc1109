f5e33a3721b55af19fd0caaeb6231622eac3fe0d
2014.felixchang <2014.felixchang@gmail.com>
2024-02-07 15:22:45 +0800

    Added some ideas for Bank class

--- Bank.java
+++ Bank.java
@@ -1,5 +1,19 @@
 public class Bank {
+    private String bankName;
+
     public static void main(String[] args) {
         // code here
+    }
+
+    public void transferMoney(int sourceAccount, int destinationAccount, double amount){
+
+    }
+
+    public void addAccount(){
+
+    }
+
+    public void removeAccount(){
+        
     }
 }
