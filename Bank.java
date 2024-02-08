0b4a4ff9f2aed6199d99168871cf5d906b2d496b
2014.felixchang <2014.felixchang@gmail.com>
2024-02-08 17:10:15 +0800

    Made changes to Bank class on how it interacts with the Account objects

--- Bank.java
+++ Bank.java
@@ -1,3 +1,5 @@
+// need to add in validation check like does the account exist before transferring money or removing account etc
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +16,8 @@ public class Bank {
 
     public static void main(String[] args) {
         Bank bank1 = new Bank("ABC Bank");
+
+        // Make 3 accounts to test
         Account account1 = new Account("111", 50.00, 0);
         Account account2 = new Account("222", 50.00, 0);
         Account account3 = new Account("333", 50.00, 0);
@@ -21,29 +25,42 @@ public class Bank {
         bank1.addAccount(account2);
         bank1.addAccount(account3);
 
-        // Check if account added
-        bank1.getAccountNums();
+        // When customer success login to their account, need to have their current loggedInAccount
+        Account loggedInAccount = account1;
 
-        bank1.transferMoney(account1, account2, 10.00);
+        // Check if account added
+        bank1.getAccountNumList();
+
+        bank1.transferMoney(loggedInAccount.getAccountNum(), "222", 10.00);
 
         // Check if transfer successful
         bank1.displayAccountInfo("111");
         bank1.displayAccountInfo("222");
     }
 
-    public void transferMoney(Account sourceAccount, Account destinationAccount, double amount){
+    public void transferMoney(String sourceAccountNum, String destinationAccountNum, double amount){
+        Account sourceAccount = getAccountObj(sourceAccountNum);
+        Account destinationAccount = getAccountObj(destinationAccountNum);
+
         if (Double.parseDouble(sourceAccount.getBalance()) < amount) {
             System.out.println("Insufficient balance to transfer funds.");
             return;
         }
-        sourceAccount.withdraw(amount);
-        destinationAccount.deposit(amount);
+        sourceAccount.setBalance( (Double.parseDouble(sourceAccount.getBalance())) - amount);
+        destinationAccount.setBalance( (Double.parseDouble(destinationAccount.getBalance())) + amount);
         // (Waiting for account to make addHistory())
         // sourceAccount.addHistory("Transfered $" + amount + " to Account Number: " + destinationAccount.getAccountNum());
         // destinationAccount.addHistory ("Received $" + amount + " from Account Number: " + sourceAccount.getAccountNum());
     }
 
-    public List<String> getAccountNums(){
+    public Account getAccountObj(String accountNum){
+        // need to check if account exists
+        int accountIndex = accountNums.indexOf(accountNum);
+        Account tempAccount = accounts.get(accountIndex);
+        return tempAccount;
+    }
+
+    public List<String> getAccountNumList(){
         System.out.println(accountNums);
         return accountNums;
     }
@@ -53,14 +70,23 @@ public class Bank {
         accountNums.add(account.getAccountNum());
     }
 
-    public void removeAccount(){
-        
+    public void removeAccount(String accountNum){
+        // need to check if account exists for it to be removed
+        int accountIndex = accountNums.indexOf(accountNum);
+        accounts.remove(accountIndex);
     }
 
     public void displayAccountInfo(String accountNum){
-        int accountIndex = accountNums.indexOf(accountNum);
-        Account tempAccount = accounts.get(accountIndex);
+        Account tempAccount = getAccountObj(accountNum);
         tempAccount.displayAccountInfo();
+    }
+
+    public void setAccountTransferLimit(String accountNum, double newLimit){
+        // TO-BE IMPLEMENTED
+        // Take in from CLI when customer enter the input and pass it onto their account's setting
+
+        // Account tempAccount = getAccount(accountNum);
+        // tempAccount.setAccountTransferLimit():
     }
 
 
