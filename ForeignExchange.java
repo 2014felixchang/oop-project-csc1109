53abd757402d39483d241ebe9776d522585f9f85
2014.felixchang <2014.felixchang@gmail.com>
2024-02-10 18:15:46 +0800

    Added a bare bones convert method for ForeignExchange

--- ForeignExchange.java
+++ ForeignExchange.java
@@ -1,3 +1,17 @@
 public class ForeignExchange {
-    
+    private double sgdToUsdRate = 0.75;
+    private double usdToSgdRate = 1.24;
+
+    public double convert(String fromCurrency, String toCurrency, double amount){
+        System.out.println("Converting " + amount + " " + fromCurrency + " to " + toCurrency);
+        
+        if(fromCurrency == "SGD" && toCurrency == "USD"){
+            return amount * sgdToUsdRate;
+        }
+        if(fromCurrency == "USD" && toCurrency == "SGD"){
+            return amount * usdToSgdRate;
+        }
+
+        return amount;
+    }
 }
